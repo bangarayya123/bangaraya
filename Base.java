beb358f53009013fa660915e2656c3b7f72dfefa
bangarayya123 <bangarayyamekala@gmail.com>
2021-10-14 23:00:57 +0530

    modified

---
+++ Base.java
@@ -0,0 +1,12 @@
+﻿package baseClass;
+
+public class Base {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("heloooo");
+System.out.println(“mani”)
+
+	}
+
+}
